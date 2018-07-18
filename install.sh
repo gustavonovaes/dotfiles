b4f5a6570c63e0b6d9a5788539e1c9c7b52cdd42
Gustavo Novaes <gustavonovaes93@gmail.com>
2018-07-18 08:59:06 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,25 +4,28 @@
 sudo rmmod pcspk
 
 # Install essential apps
-sudo pacman -S i3 i3-gaps dmenu rxvt-unicode compton rofi feh ranger w3m --no-confirm
-sudo pacman -S git pacaur zsh neofetch flameshot xautolock
+sudo pacman -S git pacaur zsh neofetch flameshot
+
+# Install i3 needed apps
+sudo pacman -S i3 i3-gaps dmenu rxvt-unicode compton rofi feh ranger w3m xautolock --no-confirm
+
+# Install alternative to i3lock
+pacaur -S betterlockscreen --noconfirm
 
 # Install Oh-My-Zsh
 sudo pacman -S curl
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Install alternative to i3lock
-pacaur -S betterlockscreen --noconfirm
-
 # Install fonts
-mkdir -p ~/.local/share/fonts
-cp fonts/* ~/.local/share/fonts/
+pacman -S otf-font-awesome --noconfirm
+pacaur -S ttf-iosevka --silent --noconfirm
 fc-cache -f
 
 # Copy setting files
 cp -r .i3 ~/
 cp -r .config/ ~/
 cp .zshrc ~/
+cp .Xresources ~/
 
 # Load Xresources
 xrdb ~/.Xresources
@@ -33,8 +36,14 @@ betterlockscreen -u $HOME/.i3/background.jpg -r 1920x1080 -b
 # Setup ranger file manager
 ranger --copy-config=all
 
+# Making directories
 mkdir -p ~/.local/bin
 
-# Install usql
+## Install User Programs
+
+# usql
 curl -Ls https://github.com/xo/usql/releases/download/v0.7.0/usql-0.7.0-linux-amd64.tar.bz2 | tar jvx
 mv usql ~/.local/bin/
+
+# VSCode
+pacaur -S --silent --noconfirm visual-studio-code-bin
